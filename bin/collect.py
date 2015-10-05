47c9d797e01006080079d458f44460fef60b604f
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2015-10-05 11:06:19 +0200

    Read session ids from file or stdin.

--- bin/collect.py
+++ bin/collect.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import radical.pilot.utils as rpu
 import radical.utils as ru
 import pandas as pd
@@ -46,12 +47,14 @@ if __name__ == '__main__':
 
     report = ru.Reporter("Collect profiling and json data to local disk.")
 
-    sessions_to_fetch = ['rp.session.netbook.mark.016708.0003',
-                     'rp.session.netbook.mark.016708.0004',
-                     'rp.session.netbook.mark.016708.0005',
-                     'rp.session.netbook.mark.016708.0010',
-                     'rp.session.netbook.mark.016708.0011',
-                     'rp.session.netbook.mark.016708.0012'
-    ]
+    session_ids = []
 
-    collect_all(sessions_to_fetch)
+    # Read from file if specified, otherwise read from stdin
+    f = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
+    for line in f:
+        session_ids.append(line.strip())
+
+    report.info("Session ids found on input:\n")
+    report.plain("%s\n" % session_ids)
+
+    collect_all(session_ids)
