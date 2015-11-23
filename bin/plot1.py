17f80c37650b6c7defd6a469b76fe8ac564c81c6
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2015-11-23 15:10:54 +0100

    Use find_preprocessed_sessions.

--- bin/plot1.py
+++ bin/plot1.py
@@ -1,10 +1,9 @@
 import os
 import sys
 import time
-import glob
 import pandas as pd
 
-from common import JSON_DIR, PICKLE_DIR
+from common import PICKLE_DIR, find_preprocessed_sessions
 
 # Global Pandas settings
 pd.set_option('display.width', 180)
@@ -164,22 +163,6 @@ def plot(tr_unit_prof_df, info_df, unit_info_df, pilot_info_df, sid):
     mp.pyplot.savefig('%s_plot1.pdf' % sid)
     mp.pyplot.close()
 
-###############################################################################
-#
-def find_sessions(json_dir):
-
-    session_paths = glob.glob('%s/rp.session.*json' % json_dir)
-    if not session_paths:
-        raise Exception("No session files found in directory %s" % json_dir)
-
-    session_files = [os.path.basename(e) for e in session_paths]
-
-    session_ids = [e.rsplit('.json')[0] for e in session_files]
-
-    print "Found sessions in %s: %s" % (json_dir, session_ids)
-
-    return session_ids
-
 
 ###############################################################################
 #
@@ -193,12 +176,14 @@ if __name__ == '__main__':
         session_ids.append(line.strip())
 
     if not session_ids:
-        session_ids = find_sessions(JSON_DIR)
-
-    unit_info_df = pd.read_pickle(os.path.join(PICKLE_DIR, 'unit_info.pkl'))
-    pilot_info_df = pd.read_pickle(os.path.join(PICKLE_DIR, 'pilot_info.pkl'))
-    tr_unit_prof_df = pd.read_pickle(os.path.join(PICKLE_DIR, 'tr_unit_prof.pkl'))
-    session_info_df = pd.read_pickle(os.path.join(PICKLE_DIR, 'session_info.pkl'))
+        session_ids = find_preprocessed_sessions()
 
     for sid in session_ids:
+        session_dir = os.path.join(PICKLE_DIR, sid)
+
+        unit_info_df = pd.read_pickle(os.path.join(session_dir, 'unit_info.pkl'))
+        pilot_info_df = pd.read_pickle(os.path.join(session_dir, 'pilot_info.pkl'))
+        tr_unit_prof_df = pd.read_pickle(os.path.join(session_dir, 'tr_unit_prof.pkl'))
+        session_info_df = pd.read_pickle(os.path.join(session_dir, 'session_info.pkl'))
+
         plot(tr_unit_prof_df, session_info_df, unit_info_df, pilot_info_df, sid)
